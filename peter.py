e0df9e42c3a7e2607924d1b13a9b5cdbc55fd1fc
Peter-Chong22 <113398388+Peter-Chong22@users.noreply.github.com>
2022-09-20 15:09:49 -0500

    Delete peter.py

--- peter.py
+++
@@ -1,9 +0,0 @@
-"Hello, My name is Peter Chong". 
-
-"Welcome to my website"
-
-read "The How many total requests have been made in the 6 months?"
-echo "the Total request made in 6 months was  
-
-read "How many total requests were made in the time period represented by the log?"
-echo "The total request made represented by the log was 
