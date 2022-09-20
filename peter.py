45e5197a051efc4dd0641d5a2c3b5dbd99ef2dc1
Peter-Chong22 <113398388+Peter-Chong22@users.noreply.github.com>
2022-09-20 13:02:54 -0500

    Update peter.py

--- peter.py
+++ peter.py
@@ -1,5 +1,12 @@
-Hello, My name is Peter Chong. 
+"Hello, My name is Peter Chong". 
 
-Welcome to my website 
+"Welcome to my website"
+
+read "The How many total requests have been made in the 6 months?"
+Echo "the Total request made in 6 months was  
+
+read "How many total requests were made in the time period represented by the log?"
+Echo "The total request made represented by the log was 
+
 
 
